25f13996b1ab73eefa43d4b9ec28f6ebc1fa4577
Andres Karapetyan <illum.andres@gmail.com>
2022-12-19 21:08:22 +0400

    Task10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе
+// показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число ");
+
+int GetSecondNumber(int number)
+{
+    return number/10%10;   
+}
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSecondNumber(number));
